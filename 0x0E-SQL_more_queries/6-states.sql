6d5de67b6c99b33d8b5c460a38d1d6ae3d57498f
Amir Rahemeto <amiroimage@gmail.com>
2023-02-15 23:18:22 -0800

    task 6

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,3 +1,5 @@
--- lists all rows of the table first_table
--- from the database hbtn_0c_0
-SELECT * FROM first_table;
+-- Creates the database hbtn_0d_usa and the table states 
+-- (in the database hbtn_0d_usa) on your MySQL server.
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+USE `hbtn_0d_usa`;
+CREATE TABLE IF NOT EXISTS `states` (`id` INT NOT NULL AUTO_INCREMENT, `name` VARCHAR(256) NOT NULL, PRIMARY KEY(`id`));
